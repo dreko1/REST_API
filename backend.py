5373d940ecb2e0b5be84cc7e4a951c4f4a38806e
David <dreko@calpoly.edu>
2021-04-07 10:35:39 -0700

    finished step 3

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 users = { 
@@ -34,7 +34,18 @@ users = {
 
 @app.route('/users')
 def get_users():
+   search_username = request.args.get('name') #accessing the value of parameter 'name'
+   if search_username :
+      subdict = {'users_list' : []}
+      for user in users['users_list']:
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
    return users
+
+#@app.route('/users')
+#def get_users():
+#   return users
 
 @app.route('/')
 def hello_world():
