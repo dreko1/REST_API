80f66cde56092eab57b3edc43276addb285bb0cc
David <dreko@calpoly.edu>
2021-04-09 09:25:48 -0700

    added new delete method

--- backend.py
+++ backend.py
@@ -70,14 +70,30 @@ def get_users():
        resp = jsonify(success=True)
        return resp
 
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
+   if request.method == 'GET':
+      if id :
+         for user in users['users_list']:
+            if user['id'] == id:
+               return user
+         return ({})
+      return users
+   elif request.method == 'DELETE':
+      if id : 
+         for user in users['users_list']:
+            if user['id'] == id:
+               users['users_list'].remove(user)
+               resp = jsonify(success=True)
+               resp.status_code = 204
+               return resp
+         resp = jsonify(success=True)
+         resp.status_code = 404
+         return resp
+      resp = jsonify(success=True)
+      resp.status_code = 404
+      return resp
+            
 
 # @app.route('/users')
 # def get_users():
